4a837596a709c0742e539b2f99204149c5e1ebbf
aydee <aydee.qm@gmail.com>
2017-09-04 17:36:51 -0500

    proyecto evaluado de start studio

---
+++ assets/js/studio.js
@@ -0,0 +1,13 @@
+
+/*let picture = ['assets/img/img-1.png', 'assets/img/img-2.png', 'assets/img/img-3.png', 'assets/img/img-4.png', 'assets/img/img-5.png', 'assets/img/img-6.png'];
+
+let div = "";
+div += "<div class='container'>"
+
+    $.grep(picture, function (value, index) {
+        $('#box-images').append(
+            div += "<div class='col-md-4'><img src='assets/img/img-" + [index + 1] + ".png'/></div>");
+        
+});
+
+div += "</div>";*/
